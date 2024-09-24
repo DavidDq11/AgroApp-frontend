fadad5ba8272de2d2c68f17b39932f6c419c9083
DavidDq11 <daviddqo123@gmail.com>
2024-09-23 19:18:12 -0500

    Registro de usuarios cnfigurado con la base de datos y el front

--- app/(guest)/registro/_form.tsx
+++ app/(guest)/registro/_form.tsx
@@ -1,199 +1,147 @@
-// 'use client'
+'use client'
 
-// import { Mascota } from '../../../types/MyTypes'
-// import { getErrorsForFields, toAuth, transformErrors } from '..//../../lib/actions'
-// import { useRouter } from 'next/navigation'
-// import { MUNDOS_ROUTE } from '../../../utils/routes'
-// import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '../../../components/ui/select'
-// import { Button } from '../../../components/ui/button'
-// import { Input } from '../../../components/ui/input'
-// import { Avatar, AvatarFallback, AvatarImage } from '../../../components/ui/avatar'
-// import { RadioGroup, RadioGroupItem } from '../../../components/ui/radio-group'
-// import { Label } from '../../../components/ui/label'
-// import { toast } from '../../../components/ui/use-toast'
-// import LoadingOverlay from '../../loading'
-// import { Usuario } from '../../../types/MyTypes'
-// import { fetcher } from '../../../utils/fetcher'
-// import useSWR from 'swr'
-// import { useState } from 'react'
-// import { useAudioPlayer } from '../../../hooks/useAudioPlayer'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { HOME_ROUTE, LOGIN_ROUTE } from '../../../utils/routes'
+import Link from 'next/link'
 
-// export default function RegisterForm() {
-//     const { data: mascotas } = useSWR<Mascota[]>(`${process.env.NEXT_PUBLIC_NESTJS_API_URL}/mascota`, fetcher)
-//     const { formData, handleChange } = useForm<Partial<Usuario>>({})
-//     const [errors, setErrors] = useState<any[]>([])
-//     const [loading, setLoading] = useState<boolean>(false)
-//     const fields = ['nombre', 'nombreUsuario', 'edad', 'grado', 'mascotaId', 'mascotaNombre']
-//     const fieldErrors = getErrorsForFields(fields, errors)
-//     const { playSound, pauseSound, stopSound } = useAudioPlayer()
+export default function RegistrationForm() {
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+    const [error, setError] = useState<string | null>(null)
+    const [successMessage, setSuccessMessage] = useState<string | null>(null) // Nuevo estado para el mensaje de éxito
+    
+    const router = useRouter()
 
-//     const router = useRouter()
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
 
-//     if (!mascotas) {
-//         return <LoadingOverlay />
-//     }
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
 
-//     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-//         event.preventDefault() // Evita que el formulario se envíe automáticamente
-//         setLoading(true)
+        console.log("Submit form data:", formData); 
+        // Validación básica
+        if (formData.password !== formData.confirmPassword) {
+            setError('Las contraseñas no coinciden');
+            return;
+        }
 
-//         try {
-//             const response = await toAuth(`${process.env.NEXT_PUBLIC_NESTJS_API_URL}/auth/register`, formData)
+        try {
+            // Realizar la petición al backend
+            const response = await fetch(`${process.env.NEXT_PUBLIC_NESTJS_API_URL}/users`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username: formData.email.split('@')[0], // Puedes usar la parte del correo como username o definir otra lógica
+                    name: `${formData.firstName} ${formData.lastName}`, // Combina nombre y apellido
+                    email: formData.email,
+                    password: formData.password,
+                }),
+            });
 
-//             if (!response.ok) {
-//                 const errorData = await response.json()
+            console.log("Response status:", response.status); // Ver si se está obteniendo una respuesta
+            
+            const data = await response.json(); // Mover aquí la obtención de datos
+            if (!response.ok) {
+                console.error('Error:', data);
+                setError(data.message || 'Error desconocido en el registro'); 
+                console.log('Backend response data:', data); // Ver los datos del backend
+                return; // Asegúrate de salir aquí si hay un error
+            }
 
-//                 if (errorData.errors) {
-//                     setErrors(transformErrors(errorData.errors))
-//                 } else {
-//                     toast({ title: '❌ Error', description: errorData.message })
-//                     throw new Error(errorData.message)
-//                 }
-//                 return
-//             }
+            // Si el registro fue exitoso, establecer el mensaje de éxito
+            console.log('✅ Registro exitoso');
+            setSuccessMessage('Registro exitoso. Serás redirigido al inicio de sesión en breve.');
+            setTimeout(() => {
+                router.push(LOGIN_ROUTE);
+            }, 2000); // Redirige después de 2 segundos
 
-//             const data = await response.json()
-//             const token = data.token
+        } catch (error) {
+            console.error('Error en el registro:', error);
+            setError('Error en el servidor');
+        }
+    };
 
-//             if (token) {
-//                 const cookieOptions = {
-//                     domain: process.env.NEXT_PUBLIC_DOMAIN,
-//                     secure: true,
-//                     httpOnly: true,
-//                     maxAge: 24 * 60 * 60,
-//                 }
-
-//                 const cookieString = `accessToken=${token}; domain=${cookieOptions.domain}; maxAge=${cookieOptions.maxAge}; secure;`
-//                 document.cookie = cookieString
-
-//                 router.push(MUNDOS_ROUTE)
-//             } else {
-//                 throw new Error('No se recibió un token en la respuesta')
-//             }
-//         } catch (error) {
-//             console.error('Error al iniciar sesión:', error)
-//             // Manejar errores si es necesario
-//         }
-
-//         setTimeout(() => {
-//             setLoading(false)
-//         }, 3000)
-//     }
-
-//     return (
-//         <form
-//             className="flex flex-col gap-y-6 w-9/12 sm:w-5/12"
-//             onSubmit={handleSubmit}>
-//             <div>
-//                 <Input
-//                     type="text"
-//                     placeholder="Nombre del alumno"
-//                     className="py-4 px-8 text-center"
-//                     onChange={(event) => handleChange('nombre', event.target.value)}
-//                     required
-//                 />
-//                 {fieldErrors['nombre'] && <small className="text-red-500">{fieldErrors['nombre']}</small>}
-//             </div>
-
-//             <div>
-//                 <Input
-//                     type="number"
-//                     placeholder="Edad"
-//                     className="py-4 px-8 text-center"
-//                     min="1"
-//                     onChange={(event) => handleChange('edad', event.target.value)}
-//                     required
-//                 />
-//                 {fieldErrors['edad'] && <small className="text-red-500">{fieldErrors['edad']}</small>}
-//             </div>
-
-//             <div>
-//                 <Select
-//                     name="grado"
-//                     onValueChange={(value) => handleChange('grado', value)}
-//                     required>
-//                     <SelectTrigger>
-//                         <SelectValue placeholder="Grado" />
-//                     </SelectTrigger>
-//                     <SelectContent>
-//                         <SelectItem value="1">1</SelectItem>
-//                         <SelectItem value="2">2</SelectItem>
-//                         <SelectItem value="3">3</SelectItem>
-//                         <SelectItem value="4">4</SelectItem>
-//                         <SelectItem value="5">5</SelectItem>
-//                     </SelectContent>
-//                 </Select>
-//                 {fieldErrors['grado'] && <small className="text-red-500">{fieldErrors['grado']}</small>}
-//             </div>
-
-//             <div>
-//                 <Input
-//                     type="text"
-//                     placeholder="Colegio"
-//                     className="py-4 px-8 text-center"
-//                     onChange={(event) => handleChange('colegio', event.target.value)}
-//                     required
-//                 />
-//                 {fieldErrors['colegio'] && <small className="text-red-500">{fieldErrors['colegio']}</small>}
-//             </div>
-
-//             <div>
-//                 <Input
-//                     type="text"
-//                     placeholder="Nombre del personaje"
-//                     className="py-4 px-8 text-center"
-//                     onChange={(event) => handleChange('nombreUsuario', event.target.value)}
-//                     required
-//                 />
-//                 {fieldErrors['nombreUsuario'] && <small className="text-red-500">{fieldErrors['nombreUsuario']}</small>}
-//             </div>
-
-//             <div>
-//                 <Label className="my-4 text-center block text-white">Seleccione un acompañante</Label>
-//                 <RadioGroup
-//                     onValueChange={(value) => handleChange('mascotaId', value)}
-//                     required>
-//                     <div className="flex items-center justify-center gap-4">
-//                         {mascotas?.map((mascota: Mascota, index: number) => (
-//                             <div
-//                                 key={mascota.id}
-//                                 className="flex flex-col-reverse items-center gap-2">
-//                                 <RadioGroupItem
-//                                     className="hover:opacity-80"
-//                                     value={mascota.id}
-//                                     id={mascota.id}
-//                                 />
-//                                 <Label
-//                                     htmlFor={mascota.id}
-//                                     className="hover:cursor-pointer hover:opacity-60">
-//                                     <Avatar className="size-20">
-//                                         <AvatarImage src={`${process.env.NEXT_PUBLIC_NESTJS_ASSETS}/${mascota.foto}`} />
-//                                         <AvatarFallback>MASCOTA</AvatarFallback>
-//                                     </Avatar>
-//                                 </Label>
-//                             </div>
-//                         ))}
-//                     </div>
-//                 </RadioGroup>
-//                 {fieldErrors['mascotaId'] && <small className="text-red-500">{fieldErrors['mascotaId']}</small>}
-//             </div>
-
-//             <div>
-//                 <Input
-//                     type="text"
-//                     placeholder="Nombre del acompañante"
-//                     className="py-4 px-8 text-center"
-//                     onChange={(event) => handleChange('mascotaNombre', event.target.value)}
-//                     required
-//                 />
-//                 {fieldErrors['mascotaNombre'] && <small className="text-red-500">{fieldErrors['mascotaNombre']}</small>}
-//             </div>
-
-//             <Button
-//                 disabled={loading}
-//                 onMouseEnter={() => playSound('phoneShowed')}>
-//                 {loading ? 'Guardando...' : 'Registrarse'}
-//             </Button>
-//         </form>
-//     )
-// }
+    return (
+        <>
+            <Link 
+                href={HOME_ROUTE}
+                className="absolute top-4 left-4 bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded transition duration-300 shadow-md hover:shadow-lg z-10"
+            >
+                Inicio
+            </Link>
+            <form className="flex flex-col gap-y-4 w-9/12 sm:w-5/12" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    name="firstName"
+                    placeholder="Nombre"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                    required
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <input
+                    type="text"
+                    name="lastName"
+                    placeholder="Apellido"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                    required
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Correo electrónico"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                    value={formData.password}
+                    onChange={handleChange}
+                    required
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirmar contraseña"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
+                    required
+                    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                />
+                {error && <small className="text-red-500">{error}</small>}
+                {successMessage && <small className="text-green-500">{successMessage}</small>} {/* Mensaje de éxito */}
+                <button
+                    type="submit"
+                    className="bg-green-700 text-white py-2 px-6 rounded-lg shadow-lg hover:bg-green-600 hover:shadow-xl transition duration-300 transform hover:-translate-y-1 active:translate-y-0.5 font-semibold cursor-pointer tracking-wider"
+                >
+                    Registrarse
+                </button>
+                <Link
+                    href={LOGIN_ROUTE}
+                    className="bg-blue-700 text-white py-2 px-6 rounded-lg shadow-lg hover:bg-blue-600 hover:shadow-xl transition duration-300 transform hover:-translate-y-1 active:translate-y-0.5 font-semibold cursor-pointer tracking-wider flex items-center justify-center text-center w-full"
+                >
+                    Ya tengo una cuenta
+                </Link>
+            </form>
+        </>
+    )
+}
--- app/(guest)/registro/page.tsx
+++ app/(guest)/registro/page.tsx
@@ -1,15 +1,33 @@
 import { Metadata } from 'next'
-import RegisterForm from './_form'
+import RegistrationForm from './_form'
 
 export const metadata: Metadata = {
     title: 'Registro',
 }
 
-export default function RegistroPage() {
+export default function RegistrationPage() {
     return (
         <>
             <div className="lg:max-w-5xl w-full mx-auto text-left lg:mt-0 grow flex flex-col items-center justify-center gap-y-10 relative">
-                <RegisterForm />
+                <h1 className="
+                    text-6xl
+                    text-center
+                    font-bold
+                    font-edu
+                    text-white
+                    shadow-lg
+                    bg-gradient-to-r from-blue-700 to-cyan-500
+                    bg-clip-text
+                    text-transparent
+                    animate-pulse
+                    tracking-wider
+                    mb-8
+                    [text-shadow:2px_2px_4px_rgba(0,0,0,0.5)]
+                ">
+                    Registro
+                </h1>
+
+                <RegistrationForm />
             </div>
         </>
     )
